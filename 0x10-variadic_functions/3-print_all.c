ed5882bba37cd27cd3acf08d4cca86982218a149
Sunday Oluwasegun Omotayo <sunnnyomotee@gmail.com>
2022-08-19 01:13:40 -0700

    task variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,7 +6,6 @@
  * print_int - prints int
  * @list: arguments from print_all
  */
-
 void print_int(va_list list)
 {
 	printf("%d", va_arg(list, int));
@@ -16,7 +15,6 @@ void print_int(va_list list)
  * print_float - prints float
  * @list: arguments from print_all
  */
-
 void print_float(va_list list)
 {
 	printf("%f", va_arg(list, double));
@@ -26,21 +24,21 @@ void print_float(va_list list)
  * print_char - prints int
  * @list: arguments from print_all
  */
-
 void print_char(va_list list)
 {
 	printf("%c", va_arg(list, int));
 }
+
 /**
  * print_str - prints string
  * @list: arguments from print_all
  */
-
 void print_str(va_list list)
 {
 	char *s = va_arg(list, char *);
 
 	s == NULL ? printf("(nil)") : printf("%s", s);
+
 }
 
 /**
@@ -61,6 +59,7 @@ void print_all(const char * const format, ...)
 		{ "s", print_str },
 		{NULL, NULL}
 	};
+
 	va_start(list, format);
 	while (format && format[i])
 	{
