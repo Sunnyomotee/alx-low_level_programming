a8618830179c27058a650d6ff90c90ddd9feffc2
Sunday Oluwasegun Omotayo <sunnnyomotee@gmail.com>
2022-08-20 00:54:22 -0700

    singly linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
+void __attribute__((constructor)) hare(void);
 /**
  * before_main - function that executes before the main function
  * Return: nothing
  */
 
-void before_main(void) __attribute__ ((constructor));
-void before_main(void)
+void hare(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
